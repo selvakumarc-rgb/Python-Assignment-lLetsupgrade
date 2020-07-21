16afd6f3a882b9672a96270c6408264992d8210f
selvakumarc-rgb <60534829+selvakumarc-rgb@users.noreply.github.com>
2020-07-21 14:50:13 +0530

    Add files via upload

---
+++ NEW-PYTHONE-DAY2 (1).py
@@ -0,0 +1,129 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+print ('hello world')
+
+
+# In[3]:
+
+
+print ("hello \n python")
+
+
+# In[4]:
+
+
+print("python's world")
+
+
+# In[10]:
+
+
+name ="Selva"
+marks="90"
+age="45"
+print("The name of person is 10%s", "marks is" %10.2f ,"age is %10d"(name,marks,age))
+
+
+# In[7]:
+
+
+name="selva"
+marks="90"
+age="45"
+print(f "The name of person is" {name}, "the marks is", {marks}"age is", {age})
+
+
+# In[8]:
+
+
+name="xyz"
+marks=90.86
+age=20
+print("The name of person", name, "marks is",marks,"age is",age)
+
+
+# In[15]:
+
+
+name="xyz"
+marks=90.86
+age=20
+
+print(f "The name of person is {name} the marks is {90.86} age is {20}")
+
+
+# In[19]:
+
+
+name ="xyz"
+marks=90.86
+age=20
+print(f "The name of person is {name} the mark is {marks} age is {age}")
+
+
+# In[20]:
+
+
+#Python variable assignment
+x=y=z=20
+
+10/2
+10//3
+10%3
+
+
+# In[21]:
+
+
+comparision operator
+
+a=10
+b=20
+a==b
+
+
+# In[22]:
+
+
+Assignment operator
+a=a+b
+a+=b
+
+a=a-b
+a-=b
+
+a=a*b
+a*=b
+
+a=a/b
+a/=b
+
+a=a**b
+
+
+a=10
+b=10
+a==b
+
+
+# In[23]:
+
+
+a=10
+b=10
+a==b
+
+
+# In[24]:
+
+
+x=1.5
+y=1.5
+x==y
+
+x is y
+
